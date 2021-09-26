bc054d36f13ade26159761a39ff76d3ed2fd7a8b
Haidar Rais <abdillah.fams@gmail.com>
2021-09-26 23:31:04 +0700

    all

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -13,66 +13,6 @@ Dashboard
 @endsection
 @section('content')
 <!-- Main Content -->
-<<<<<<< HEAD
-<div class="row">
-    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-      <div class="card card-statistic-1">
-        <div class="card-icon bg-primary">
-          <i class="fas fa-inbox"></i>
-        </div>
-        <div class="card-wrap">
-          <div class="card-header">
-            <h4>Surat Masuk</h4>
-          </div>
-          <div class="card-body">
-            {{$inboxes->count()}}
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-      <div class="card card-statistic-1">
-        <div class="card-icon bg-danger">
-          <i class="far fa-paper-plane"></i>
-        </div>
-        <div class="card-wrap">
-          <div class="card-header">
-            <h4>Surat Keluar</h4>
-          </div>
-          <div class="card-body">
-            0
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-      <div class="card card-statistic-1">
-        <div class="card-icon bg-warning">
-          <i class="far fa-file"></i>
-        </div>
-        <div class="card-wrap">
-          <div class="card-header">
-            <h4>Tipe Surat</h4>
-          </div>
-          <div class="card-body">
-            {{$types->count()}}
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-      <div class="card card-statistic-1">
-        <div class="card-icon bg-success">
-          <i class="fas fa-circle"></i>
-        </div>
-        <div class="card-wrap">
-          <div class="card-header">
-            <h4>Jumlah Unit</h4>
-          </div>
-          <div class="card-body">
-            39
-          </div>
-=======
 
 
 <div class="section-body">
@@ -101,7 +41,6 @@ Dashboard
           <p class="lead pb-0 mb-0 font-weight-bold text-primary"><?= $inbox ?></p>
           <!-- </div> -->
           <div>Surat Keluar</div>
->>>>>>> c283be542b2a32daa0e33716d79c08c47751f79a
         </div>
       </div>
       @hasrole('sekertaris')
@@ -120,12 +59,7 @@ Dashboard
       @endhasrole
 
     </div>
-<<<<<<< HEAD
-  </div>
-@endsection
-=======
-   
+
   </div>
 </div>
 @endsection
->>>>>>> c283be542b2a32daa0e33716d79c08c47751f79a
