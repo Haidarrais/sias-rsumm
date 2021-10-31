e7a6477d56eafc7f173ad2cbbdd79b51dfd2057d
Haidar Rais <haidarraisabd@gmail.com>
2021-11-01 02:34:42 +0700

    sidebar

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -20,62 +20,72 @@
 </head>
 
 <body>
-  <div id="app">
-    <div class="main-wrapper">
-      @include('sweetalert::alert')
-      @include('partials.navbar')
-      @include('partials.sidebar')
-      @php
-      $paths = explode('/', Request::path());
-      $len = count($paths)-1;
-      if (Request::path() != '/') {
-      $path = ucfirst($paths[$len]);
-      }else {
-      $path = 'Dashboard';
-      }
-      @endphp
-      <div class="main-content">
-        <section class="section">
-          <div class="section-header">
-            <h1>@yield('header')</h1>
-            <div class="section-header-breadcrumb">
-              {{-- @if (url()->full()=='http://127.0.0.1:8000')
-                  <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-              @else --}}
-              <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-              @if (Request::path() != "/")
-              @foreach ($paths as $key => $path )
-              <div class="breadcrumb-item">{{ucfirst($path)}}</div>
-              @endforeach
-              @endif
-              {{-- @endif --}}
+    <div id="app">
+        <div class="main-wrapper">
+        @include('sweetalert::alert')
+        @include('partials.navbar')
+        @include('partials.sidebar')
+        @php
+        $paths = explode('/', Request::path());
+        $len = count($paths)-1;
+        if (Request::path() != '/') {
+        $path = ucfirst($paths[$len]);
+        }else {
+        $path = 'Dashboard';
+        }
+        @endphp
+        <div class="main-content">
+            <section class="section">
+            <div class="section-header">
+                <h1>@yield('header')</h1>
+                <div class="section-header-breadcrumb">
+                {{-- @if (url()->full()=='http://127.0.0.1:8000')
+                    <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                @else --}}
+                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                @if (Request::path() != "/")
+                @foreach ($paths as $key => $path )
+                <div class="breadcrumb-item">{{ucfirst($path)}}</div>
+                @endforeach
+                @endif
+                {{-- @endif --}}
+                </div>
             </div>
-          </div>
-          @yield('content')
-        </section>
-      </div>
-      @include('partials.footer')
+            @yield('content')
+            </section>
+        </div>
+        @include('partials.footer')
+        </div>
     </div>
-  </div>
-  @yield('modal')
-  <!-- General JS Scripts -->
-  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-  <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
-  <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap4.min.js"></script>
-  <script src="{{asset('/assets/js/stisla.js')}}"></script>
+    @yield('modal')
+    <!-- General JS Scripts -->
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap4.min.js"></script>
+    <script src="{{asset('/assets/js/stisla.js')}}"></script>
 
-  <!-- JS Libraies -->
+    <!-- JS Libraies -->
+    <script>
+        var url = window.location.pathname;
 
-  <!-- Template JS File -->
-  <script src="{{asset('/assets/js/scripts.js')}}"></script>
-  <script src="{{asset('/assets/js/custom.js')}}"></script>
-  @yield('script')
+        $('.sidebar-menu a').each(function() {
+            var href = $(this).attr('href');
+            if (url.indexOf(href) > -1) {
+                $('.sidebar-menu li').removeClass('active');;
+                $(this).parent('li').addClass('active');
+            }
+        });
+    </script>
+    <!-- Template JS File -->
+    <script src="{{asset('/assets/js/scripts.js')}}"></script>
+    <script src="{{asset('/assets/js/custom.js')}}"></script>
+    @yield('script')
 
-  <!-- Page Specific JS File -->
+    <!-- Page Specific JS File -->
 </body>
 
 </html>
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -9,14 +9,14 @@
     <ul class="sidebar-menu">
         <li class="active"><a class="nav-link" href="/"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
         @role('superadmin')
-            <li class="menu-header">Manajemen</li>
-            <li><a class="nav-link" href="/user"><i class="fas fa-user-friends"></i> <span>User</span></a></li>
+        <li class="menu-header">Manajemen</li>
+        <li><a class="nav-link" href="/user"><i class="fas fa-user-friends"></i> <span>User</span></a></li>
         @endrole
         @unlessrole('superadmin')
         <li><a class="nav-link" href="/inbox"><i class="fas fa-inbox"></i> <span>Surat Masuk</span></a></li>
         <li><a class="nav-link" href="/outbox"><i class="fas fa-paper-plane"></i> <span>Surat Keluar</span></a></li>
         @hasrole('admin')
-        <li><a class="nav-link" href="{{route('notif.memo')}}"><i class="fas fa-comments"></i> <span>Memo</span>
+        <li><a class="nav-link" href="/notifMemo"><i class="fas fa-comments"></i> <span>Memo</span>
             @if(count($notifications)>0)<span class="badge badge-light">{{count($notifications)}}</span>@endif</a></li>
         <li class="menu-header">Manajemen</li>
         <li><a class="nav-link" href="/type"><i class="fas fa-folder"></i> <span>Jenis Surat</span></a></li>
@@ -25,10 +25,5 @@
         <li><a class="nav-link" href="/laporan"><i class="fas fa-chart-bar"></i> <span>Laporan</span></a></li>
         @endunlessrole
     </ul>
-    <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-      <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
-        <i class="fas fa-rocket"></i> Documentation
-      </a>
-    </div>
   </aside>
 </div>
